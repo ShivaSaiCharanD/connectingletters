ade72af90b895592be943222ee6c7d1b2b9a30fa
Shiva Sai Charan Dodda <shivasaicharandodda@gmail.com>
2024-06-04 21:23:00 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import logo from './logo.svg';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Game1 from './components/Game1.jsx';
-import Game2 from './components/Game2.jsx';
+// import Game2 from './components/Game2.jsx';
+import Game3 from './components/Game3';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Game2/>
+      <Game3/>
     </div>
   );
 }
--- src/components/Game3.jsx
+++ src/components/Game3.jsx
@@ -1,198 +1,265 @@
-import React, { useEffect, useRef, useState } from 'react';
-import * as PIXI from 'pixi.js';
-import data from '../cordinates.json';
-import data1 from '../newcor.json';
-import confetti from 'canvas-confetti';
-
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Application,
+  Assets,
+  Sprite,
+  Text,
+  TextStyle,
+  Graphics,
+} from "pixi.js";
+import data1 from "../cordinates.json";
+import wordsArray from "../words.json";
+import confetti from "canvas-confetti";
 
 export default function Game3() {
-    const appRef = useRef(null);
-    const [stack, setStack] = useState([]);
-    const words = ['at', 'it', 'to', 'me', 'go'];
-    useEffect(() => {
-        (async () => {
-            const app = new PIXI.Application();
-            appRef.current = app;
-            await app.init({ background: "#000", width: 1270, height: 720, antialias: true, view: document.getElementById('board') })
-            const texture = await PIXI.Assets.load('threads.jpg');
+  const appRef = useRef(null);
+  const [stack, setStack] = useState([]);
+  const [tries, setTries] = useState(0);
+  const [show, setShow] = useState(1);
+  const [counter, setCounter] = useState(0);
+  const words = wordsArray[`session1`][`item${show}`];
 
-            const sprite = new PIXI.Sprite(texture);
-            sprite.scale.x = 2;
-            sprite.scale.y = 1.5;
-            sprite.x = app.screen.width / 2;
-            sprite.y = app.screen.height / 2;
-            sprite.anchor.set(0.5, 0.5);
-            app.stage.addChild(sprite);
+  const handleNext = () => {
+    setTries(0);
+    setCounter(0);
+    setShow((show) => show + 1);
+  };
+  useEffect(() => {
+    (async () => {
+      const app = new Application();
+      appRef.current = app;
 
-            const main_text = new PIXI.Text({
-                text: 'Connect the letters to form a word',
-                style: new PIXI.TextStyle({
-                    fill:"#FFF"
-                })
-            });
-            const wrong_right = new PIXI.Text( {
-                text: ' ',
-                
-                style: new PIXI.TextStyle({
-                  fontFamily: 'Arial',
-                  fontSize: 30,
-                  fill: "#D2042D", // Text color: white 
-                  align: 'center',
-                }),
-              });
-            main_text.x = app.screen.width / 4;
-            main_text.y = 50;
-            // main_text.anchor = 0.5;
-            app.stage.addChild(main_text);
-            wrong_right.x = app.screen.width / 4;
-            wrong_right.y = 20;
-            app.stage.addChild(wrong_right);
+      let screenSize = {};
+      if (window.innerHeight < window.innerWidth) {
+        screenSize = {
+          width: window.innerHeight * (4 / 3),
+          height: window.innerHeight * 0.8,
+        };
+      } else {
+        screenSize = {
+          width: window.innerWidth * 0.9,
+          height: window.innerWidth * (3 / 4) * 0.8,
+        };
+      }
+      await app.init({
+        // background: "#000",
+        resolution: window.devicePixelRatio || 1, // Use device pixel ratio for better quality
+        autoDensity: true,
+        antialias: true,
+        canvas: document.getElementById("board"),
+        ...screenSize,
+      });
 
-            for (let index = 0; index < data.left.length; index++) {
-                const leftLetter = words[index][0];
-                const rightLetter = words[index][1];
+      app.renderer.resize(screenSize.width, screenSize.height);
 
-                const leftText = new PIXI.Text( {
-                    text: leftLetter,
-                    style : new PIXI.TextStyle({
-                        fontFamily: 'Arial',
-                    fontSize: 40,
-                    fill: "#000",
-                    align: 'center'
-                    })
-                    
-                });
+      const texture = await Assets.load(`/template${show}.jpg`);
 
-                const rightText = new PIXI.Text( {
-                    text: rightLetter,
-                    style : new PIXI.TextStyle({
-                        fontFamily: 'Arial',
-                    fontSize: 40,
-                    fill: "#000",
-                    align: 'center'
-                    })
-                });
+      let scalingFactor = 1;
+      if (window.innerHeight < window.innerWidth)
+        scalingFactor = (app.screen.height * 0.9) / texture.frame.height;
+      else scalingFactor = (app.screen.width * 0.8) / texture.frame.width;
 
-                // const xLeft = data.left[index].x * 2 + (1280 - 1000) / 2 - 20;
-                // const yLeft = data.left[index].y * 1.5 + (720 - 338 * 1.5) / 2 + 10;
-                // const xRight = data.right[index].x * 2 + (1280 - 1000) / 2 + 20;
-                // const yRight = data.right[index].y * 1.5 + (720 - 338 * 1.5) / 2 - 10;
+      const sprite = new Sprite(texture);
+      sprite.scale = scalingFactor;
+      const Padding = {
+        x: (app.screen.width - sprite.width) / 2,
+        y: (app.screen.height - sprite.height) / 2,
+      };
+      sprite.x = Padding.x;
+      sprite.y = Padding.y;
+      // sprite.x = app.screen.width / 2;
+      // sprite.y = app.screen.height / 2;
+      // sprite.anchor.set(0.5, 0.5);
+      app.stage.addChild(sprite);
 
-                const xLeft = data1[index+1].left.x*2 + (1280 - 1000) / 2 - 20;
-                const yLeft = data1[index+1].left.y * 1.5 + (720 - 338 * 1.5) / 2 + 10;
-                const xRight = data1[index+1].right.x * 2 + (1280 - 1000) / 2 + 20;
-                const yRight = data1[index+1].right.y * 1.5 + (720 - 338 * 1.5) / 2 + 10;
+      // const main_text = new Text({
+      //   text: "Connect the letters to form a word",
+      //   style: new TextStyle({
+      //     fill: "#FFF"
+      //   })
+      // });
+      // const wrong_right = new Text({
+      //   text: " ",
 
-                const circleLeft = new PIXI.Graphics()
-                    .circle(xLeft, yLeft, 30)
-                    .fill('#FFF')
-                    .stroke({ color: '#000', width: 2 });
+      //   style: new TextStyle({
+      //     fontFamily: "Arial",
+      //     fontSize: 30,
+      //     fill: "#D2042D", // Text color: white
+      //     align: "center"
+      //   })
+      // });
+      // main_text.x = app.screen.width / 4;
+      // main_text.y = 50;
+      // // main_text.anchor = 0.5;
+      // app.stage.addChild(main_text);
+      // wrong_right.x = app.screen.width / 4;
+      // wrong_right.y = 20;
+      // app.stage.addChild(wrong_right);
+      const voice =(letter)=> {
+        var msg = new SpeechSynthesisUtterance(letter);
+        window.speechSynthesis.speak(msg);
+      }
+      for (let i = 0; i < 5; i++) {
+        const leftLetter = words[i][0];
+        const rightLetter = words[i][1];
 
-                const circleRight = new PIXI.Graphics()
-                    .circle(xRight, yRight, 30)
-                    .fill('#FFF')
-                    .stroke({ color: '#000', width: 2 });
+        const leftText = new Text({
+          text: leftLetter,
+          style: new TextStyle({
+            fontFamily: "Arial",
+            fontSize: 50 * scalingFactor,
+            fill: "#000",
+            align: "center",
+          }),
+        });
 
-                leftText.x = xLeft - 10;
-                leftText.y = yLeft - 20;
+        const rightText = new Text({
+          text: rightLetter,
+          style: new TextStyle({
+            fontFamily: "Arial",
+            fontSize: 50 * scalingFactor,
+            fill: "#000",
+            align: "center",
+          }),
+        });
 
-                rightText.x = xRight - 10;
-                rightText.y = yRight - 20;
+        const xLeft =
+          (data1[`template${show}`][i + 1].left.x - 10) * scalingFactor +
+          Padding.x;
+        const yLeft =
+          data1[`template${show}`][i + 1].left.y * scalingFactor + Padding.y;
+        const xRight =
+          (data1[`template${show}`][i + 1].right.x + 10) * scalingFactor +
+          Padding.x;
+        const yRight =
+          data1[`template${show}`][i + 1].right.y * scalingFactor + Padding.y;
 
-                circleLeft.interactive = true;
-                circleRight.interactive = true;
+        const circleLeft = new Graphics()
+          .circle(xLeft, yLeft, 40 * scalingFactor)
+          .fill("#FFF")
+          .stroke({ color: "#000", width: 2 });
 
-                circleLeft.on('pointerdown', () => {
-                    let val = handleClick(leftLetter,index,leftText,circleLeft);
-                     if(val){
-                        circleLeft.tint = 0x33ff33
-                    }else if(stack.length === 0){
-                        circleLeft.tint = undefined;
-                    }
-                });
-                circleRight.on('pointerdown', () => {
-                    let val =  handleClick(rightLetter,index , rightText ,circleRight);
-                    if(val){
-                        circleRight.tint = 0x33ff33
-                        }
-                    else if(stack.length === 0){
-                        circleRight.tint = undefined;
-                    }});
+        const circleRight = new Graphics()
+          .circle(xRight, yRight, 40 * scalingFactor)
+          .fill("#FFF")
+          .stroke({ color: "#000", width: 2 });
+        circleLeft.index = i;
+        circleRight.index = i;
+        circleLeft.display = 0;
+        circleRight.display = 1;
+        leftText.x = xLeft;
+        leftText.y = yLeft;
+        // leftText._anchor = 0;
+        leftText.anchor = 0.5;
 
-                app.stage.addChild(circleLeft);
-                app.stage.addChild(circleRight);
-                app.stage.addChild(leftText);
-                app.stage.addChild(rightText);
+        rightText.x = xRight;
+        rightText.y = yRight;
+        rightText.anchor = 0.5;
+
+        circleLeft.interactive = true;
+        circleRight.interactive = true;
+
+        circleLeft.on("pointerdown", () => {
+          let val = handleclick(circleLeft);
+          voice(leftLetter);
+        });
+        circleRight.on("pointerdown", () => {
+          let val = handleclick(circleRight);
+            voice(rightLetter);
+        });
+
+        app.stage.addChild(circleLeft);
+        app.stage.addChild(circleRight);
+        app.stage.addChild(leftText);
+        app.stage.addChild(rightText);
+      }
+      const handleclick = (Graphics) => {
+        stack.push(Graphics);
+        if (stack.length === 1) {
+          if (Graphics.display !== 0) {
+            Graphics.tint = "FF0000"
+            stack.pop()
+            resetColor(Graphics)
+            return false;
+          } 
+          Graphics.tint = "#FFFF00";
+        }
+
+        else if(Graphics.display!==stack[stack.length-2].display+1 || stack[stack.length-2].index!==Graphics.index){
+            while(stack.length!==0){
+              const elem = stack.pop();
+              resetColor(elem);
+              elem.tint = "#FF0000";
+              elem.interactive = false;
             }
+        } 
+        else if (stack.length === words[Graphics.index].length) {
+            confetti({
+              particleCount: 300,
+              spread: 90,
+              decay: 0.95,
+              scalar: 1.5,
+              ticks: 150,
+              origin: {
+                y: 0.9,
+              },
+            });
+            while(stack.length!==0){
+              const elem = stack.pop();
+              elem.interactive = false;
+              elem.tint = "#00FF00";
+            }
+            setCounter((counter) => counter + 1);
+            console.log(counter);
+            return true;
+          
+        } else {
+          stack.pop();
+          return false;
+        }
+      };
+      const resetColor = (Graphics) => {
+        setTimeout(() => {
+          Graphics.tint = undefined;
+          Graphics.interactive = true;
+        }, 800);
+      };
+    })();
+  }, [show]);
 
-            const handleClick = (letter, index,Text,Graphics) => {
-                console.log("Clicked " + letter + " at index " + index);
-                
-                // Check if the stack is empty or the index of the new letter is equal to the index of the top element
-                if (stack.length === 0 || ((index === stack[stack.length - 1].index)  && (letter !== stack[0].letter))) {
-                    // Push the new letter and its index to the stack
-                    stack.push({ letter, index ,Text ,Graphics });
-                    if(stack.length===2){
-                        confetti({               
-                            particleCount: 600,
-                            spread: 90,
-                            decay: 0.95,
-                            scalar: 1.5,
-                            ticks: 150,
-                            origin: { y: 0.9 },
-                            
-                        });
-                        stack[1].Text.text = "";
-                        stack.pop();
-                        stack[0].Text.text = "";
+  return (
+    <div>
+      {/* <h1>Connect the letters to form a word</h1>
+       */}
 
-                        stack.pop();
-                        console.log("correct");
-                        wrong_right.style.fill = "#00FF00";
-                        wrong_right.text = "correct!!";
-                        setTimeout(() => {
-                            wrong_right.text = "";
-                        }, 2000);
-                        // console.log(stack);
-                        // setStack([]);
-                        
-                        // stack[-1].text = "";
-                        
-                        
-                    }
-                    return true;
-           
-                } else {
-                    console.log("Incorrect order!");
-                    // Clear the stack if the order is incorrect
-                        console.log(stack);
-                        wrong_right.style.fill = "#D2042D";
-                        wrong_right.text = "Incorrect order!";
-                        setTimeout(() => {
-                            wrong_right.text = "";
-                        }, 2000);
-                    // setStack([]);
-                    stack[0].Graphics.tint = undefined;
-                    stack.pop();
-                    return false;
-                }
-            };
-        })();
-       
-    }, [])
-
-    
-    
-    
-    
-    
-    
-
-    return (
-        <div>
-            <div id="pixi-container">
-                <canvas id='board'></canvas>
-            </div>
+      <div
+        id="pixi-container"
+        className="d-flex flex-column"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "98vh",
+        }}
+      >
+        <div className="d-flex justify-content-around w-100">
+          <b className="fs-4" style={{color:"green"}}>Correct {counter}</b>
+          <b className="fs-4">Level {show}</b>
         </div>
-    )
+        <div className="d-flex justify-content-around w-100">
+          <b className="fs-4" style={{color:"red"}}>Tries {tries}</b>
+          <b className="fs-5">SET-A</b>
+        </div>
+        <canvas id="board"></canvas>
+        <div>
+          {counter === 5 && show !== 3 ? (
+            <button className="btn btn-dark m-2" onClick={handleNext}>
+              {" "}
+              Next
+            </button>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 }
